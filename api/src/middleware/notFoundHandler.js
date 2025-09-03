e9959e4ec49fd9d381c330a256bc63bc4fde0446
cheickh003 <147671441+cheickh003@users.noreply.github.com>
2025-09-03 22:49:43 +0000

    reprie a zero

--- api/src/middleware/notFoundHandler.js
+++
@@ -1,6 +0,0 @@
-import { NotFoundError } from './errorHandler.js';
-
-export const notFoundHandler = (req, res, next) => {
-  const error = new NotFoundError(`Cannot ${req.method} ${req.originalUrl}`);
-  next(error);
-};
